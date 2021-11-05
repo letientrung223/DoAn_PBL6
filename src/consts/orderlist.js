df3a3bcb492ac5f338c0417c82454ab12fe0f0c7
Alden Lee <khanhky245@gmail.com>
2021-11-05 16:24:39 +0700

    edit ORDER SCREEN

--- src/consts/orderlist.js
+++ src/consts/orderlist.js
@@ -1,6 +1,6 @@
-const orderList = [
+const orderlist = [
     {id :"#003" , date:"12/3/2021", status:"complete",total:"990$" },
     {id :"#004" , date:"12/4/2021", status:"complete",total:"690$" },
   ];
   
-  export default orderList;
+  export default orderlist;
--- src/screens/OrderScreen/index.js
+++ src/screens/OrderScreen/index.js
@@ -6,21 +6,52 @@ import {
   Image,
   FlatList,
   SafeAreaView,
+  Dimensions,
   TouchableOpacity,
-  
 } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 import { ScrollView } from "react-native-gesture-handler";
 import asset from "../../../assets/asset";
-import COLORS from "../../consts/colors"
-import orderList from './../../consts/orderlist';
+import COLORS from "../../consts/colors";
+import orderList from "./../../consts/orderlist";
+const { width } = Dimensions.get("screen");
+const cardWidth = width - 20;
 
-const Order = ({navigation}) => {
+const Order = ({ navigation }) => {
+  const Card = ({ order }) => {
+    return (
+      <View style={styles.card}>
+          <Text style={{ fontSize: 20, fontWeight: "normal", paddingTop: 10 }}>
+            ID: {order.id}
+          </Text>
+          <Text style={{ fontSize: 20, fontWeight: "normal", paddingTop: 10 }}>
+            DATE: {order.date}
+          </Text>
+          <Text style={{ fontSize: 20, fontWeight: "normal", paddingTop: 10 }}>
+            STATUS: {order.status}
+          </Text>
+          <Text style={{ fontSize: 20, fontWeight: "normal", paddingTop: 10 }}>
+            TOTAL: {order.total}
+          </Text>
 
+        <View
+          style={{
+            marginTop: 10,
+            marginHorizontal: 20,
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => {}}>
+            <Text> View Detail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
   return (
-   
-    <ScrollView style={{marginTop:60}}>
+    <ScrollView style={{ marginTop: 60 }}>
       <View style={styles.header}>
         <Image
           source={asset.logo}
@@ -32,44 +63,89 @@ const Order = ({navigation}) => {
         <Image
           source={require("../../../assets/images/person.png")}
           style={{ width: 370, height: 250, borderRadius: 10 }}
-          
         />
       </View>
       <SafeAreaView>
-          <TouchableOpacity onPress={()=>{navigation.navigate("User")}} 
-            style={{ backgroundColor:'#E2E2E2',height:50,padding:10,marginHorizontal:10,marginTop:5}} >
-          <View style={{flexDirection: "row",alignItems: "center"}}>
-            <Ionicons name='home' size={26} color="black" /> 
-            <Text style={{fontSize:24}}> Dashboard</Text>
-          </View> 
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("User");
+          }}
+          style={{
+            backgroundColor: "#E2E2E2",
+            height: 50,
+            padding: 10,
+            marginHorizontal: 10,
+            marginTop: 5,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="home" size={26} color="black" />
+            <Text style={{ fontSize: 24 }}> Dashboard</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{}} 
-            style={{ backgroundColor:COLORS.grey,height:50,padding:10,marginHorizontal:10,marginTop:5}} >
-          <View style={{flexDirection: "row",alignItems: "center"}}>
-            <Ionicons name='cart' size={26} color="black" /> 
-            <Text style={{fontSize:24}}> Order</Text>
-          </View> 
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            backgroundColor: COLORS.grey,
+            height: 50,
+            padding: 10,
+            marginHorizontal: 10,
+            marginTop: 5,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="cart" size={26} color="black" />
+            <Text style={{ fontSize: 24 }}> Order</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{navigation.navigate("AccountDetail")}} 
-            style={{ backgroundColor:'#E2E2E2' ,height:50,padding:10,marginHorizontal:10,marginTop:5}} >
-          <View style={{flexDirection: "row",alignItems: "center"}}>
-            <Ionicons name='person' size={26} color="black" /> 
-            <Text style={{fontSize:24}}> Account Detail</Text>
-          </View> 
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("AccountDetail");
+          }}
+          style={{
+            backgroundColor: "#E2E2E2",
+            height: 50,
+            padding: 10,
+            marginHorizontal: 10,
+            marginTop: 5,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="person" size={26} color="black" />
+            <Text style={{ fontSize: 24 }}> Account Detail</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{navigation.navigate("ChangePassword")}} 
-            style={{ backgroundColor:'#E2E2E2',height:50,padding:10,marginHorizontal:10,marginTop:5}} >
-          <View style={{flexDirection: "row",alignItems: "center"}}>
-            <Ionicons name='key' size={26} color="black" /> 
-            <Text style={{fontSize:24}}> Change Password</Text>
-          </View> 
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("ChangePassword");
+          }}
+          style={{
+            backgroundColor: "#E2E2E2",
+            height: 50,
+            padding: 10,
+            marginHorizontal: 10,
+            marginTop: 5,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="key" size={26} color="black" />
+            <Text style={{ fontSize: 24 }}> Change Password</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{}} 
-            style={{ backgroundColor:'#E2E2E2',height:50,padding:10,marginHorizontal:10,marginTop:5}} >
-          <View style={{flexDirection: "row",alignItems: "center"}}>
-            <Ionicons name='log-out' size={26} color="black" /> 
-            <Text style={{fontSize:24}}> Log Out</Text>
-          </View> 
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            backgroundColor: "#E2E2E2",
+            height: 50,
+            padding: 10,
+            marginHorizontal: 10,
+            marginTop: 5,
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="log-out" size={26} color="black" />
+            <Text style={{ fontSize: 24 }}> Log Out</Text>
+          </View>
         </TouchableOpacity>
         <Text
           style={{
@@ -81,10 +157,13 @@ const Order = ({navigation}) => {
         >
           Order
         </Text>
-
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={orderList}
+          renderItem={({ item }) => <Card order={item} />}
+        />
       </SafeAreaView>
     </ScrollView>
-    
   );
 };
 const styles = StyleSheet.create({
@@ -111,29 +190,38 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
   },
-  textInput:{
+  textInput: {
     height: 40,
-    
+
     borderRadius: 10,
     margin: 12,
     borderWidth: 1,
     paddingHorizontal: 10,
-    backgroundColor:"#FFFFFF",
+    backgroundColor: "#FFFFFF",
   },
   radiocontainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom:10,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 10,
   },
-  text:{
+  text: {
     fontSize: 20,
     fontWeight: "normal",
     paddingTop: 10,
-    paddingLeft: 10
+    paddingLeft: 10,
   },
-
-
+  card: {
+    height: 200,
+    width: cardWidth,
+    paddingLeft:10,
+    marginLeft: 10,
+    marginBottom: 5,
+    marginTop: 20,
+    borderRadius: 15,
+    elevation: 13,
+    justifyContent:"flex-start",
+    backgroundColor: COLORS.grey_light,
+  },
 });
 export default Order;
-
